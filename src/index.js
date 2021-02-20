1d3bda40b926cad5fade6791a8ddaa342be30cd6
Daegudude <k3hppk@gmail.com>
2021-02-20 22:26:59 +0900

    Update index.js file to get basic searching work
    It updates index.js file to get basic search for the weather of the city
    to work.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("yo what?");
+import "../style.css";
+import { checkStatus, getWeatherJson, getPrimaryWeatherInfo } from "./weather";
+import showCity from "./showCity";
+
+const searchCityBtn = document.querySelector(".search-city-btn");
+
+function getUserInput() {
+  return document.querySelector('input[name="search-city"]').value;
+}
+
+searchCityBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const cityToFind = getUserInput();
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityToFind}&appid=962edb7f9ab1add3416718398c95a830`
+  )
+    .then(checkStatus)
+    .then(getWeatherJson)
+    .then(getPrimaryWeatherInfo)
+    .then((primaryInfo) => {
+      const cities = document.querySelector(".cities");
+      cities.appendChild(showCity(primaryInfo));
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});

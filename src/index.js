0ed572c841c4f64b914a13e111ceff33408c7d64
Daegudude <k3hppk@gmail.com>
2021-02-21 22:30:15 +0900

    Update to convert temperature in display
    When convert switch is clicked, it changes the temperature unit in the
    display as well.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { checkStatus, getWeatherJson, getPrimaryWeatherInfo } from "./weather";
 import City from "./city";
 import Error from "./error";
 import { resetSearchInput, getUserInput } from "./helper";
+import TempUnit from "./tempUnit";
+
+const tempUnit = new TempUnit();
+tempUnit.start();
 
 const error = new Error();
 
@@ -10,9 +14,10 @@ const searchCityBtn = document.querySelector(".search-city-btn");
 
 searchCityBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  console.log(tempUnit.getCheckedUnit());
   const cityToFind = getUserInput();
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${cityToFind}&units=metric&appid=962edb7f9ab1add3416718398c95a830`
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityToFind}&units=${tempUnit.getCheckedUnit()}&appid=962edb7f9ab1add3416718398c95a830`
   )
     .then(checkStatus)
     .then(getWeatherJson)

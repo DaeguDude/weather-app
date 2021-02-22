340869f9df1ccc7df3e4566d9985a1c960a1f5f2
Daegudude <k3hppk@gmail.com>
2021-02-22 14:09:26 +0900

    Make entry point of the code

--- src/index.js
+++ src/index.js
@@ -5,32 +5,38 @@ import Error from "./error";
 import { resetSearchInput, getUserInput } from "./helper";
 import TempUnit from "./tempUnit";
 
-const tempUnit = new TempUnit();
-tempUnit.start();
+function startApp() {
+  const tempUnit = new TempUnit();
+  tempUnit.start();
+  const error = new Error();
 
-const error = new Error();
+  activateSearchCityBtn(error, tempUnit);
+}
 
-const searchCityBtn = document.querySelector(".search-city-btn");
+function activateSearchCityBtn(error, tempUnit) {
+  const searchCityBtn = document.querySelector(".search-city-btn");
+  searchCityBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const cityToFind = getUserInput();
+    const unit = tempUnit.getCheckedUnit();
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityToFind}&units=${unit}&appid=962edb7f9ab1add3416718398c95a830`
+    )
+      .then(checkStatus)
+      .then(getWeatherJson)
+      .then(getPrimaryWeatherInfo)
+      .then((primaryInfo) => {
+        const cities = document.querySelector(".cities");
+        const city = new City(primaryInfo);
 
-searchCityBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const cityToFind = getUserInput();
-  const unit = tempUnit.getCheckedUnit();
-  fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${cityToFind}&units=${unit}&appid=962edb7f9ab1add3416718398c95a830`
-  )
-    .then(checkStatus)
-    .then(getWeatherJson)
-    .then(getPrimaryWeatherInfo)
-    .then((primaryInfo) => {
-      const cities = document.querySelector(".cities");
-      const city = new City(primaryInfo);
+        cities.appendChild(city.render(unit));
+        error.hide();
+        resetSearchInput();
+      })
+      .catch(function (err) {
+        error.show();
+      });
+  });
+}
 
-      cities.appendChild(city.render(unit));
-      error.hide();
-      resetSearchInput();
-    })
-    .catch(function (err) {
-      error.show();
-    });
-});
+startApp();
